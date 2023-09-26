03376d24f1be3c2f5b302010ed3f80143f668cfe
KevinKipkemei <42734616+KevinKipkemei@users.noreply.github.com>
2023-09-26 16:06:59 +0300

    1 file modified

--- src/pages/Listing/Listing.tsx
+++ src/pages/Listing/Listing.tsx
@@ -7,6 +7,7 @@ const Listing = () => {
         Listings
       </h3>
       <p>Experienced technical difficulties with Docker while trying to set up an instance.</p>
+      <p>Tried finding a solution for a whole day but couldn't.</p>
     </div>
   )
 }
